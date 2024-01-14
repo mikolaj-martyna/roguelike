b93df4b5255fbf2b07685393e83e06460295bc21
ENIX1701 <enix1701@gmail.com>
2024-01-14 12:01:34 +0100

    Add Clockwork Dragon

--- src/main/java/pl/umcs/entities/monsters/ClockworkDragon.java
+++ src/main/java/pl/umcs/entities/monsters/ClockworkDragon.java
@@ -1,3 +1,23 @@
 package pl.umcs.entities.monsters;
 
-public class DuneDrake {}
+import pl.umcs.entities.Entity;
+import pl.umcs.entities.Property;
+
+public class ClockworkDragon extends Entity {
+    public ClockworkDragon() {
+        super();
+
+        // GameElement
+        setName("Clockwork Dragon");
+        setSymbol('o');
+        setDescription("Towering monstrosity, combining intricate brass gears, whirring mechanisms, and bronze plating to form a majestic yet fearsome automaton resembling the mythical dragon.");
+
+        // Entity
+        setHealth(Property.builder().baseline(16).current(16).multiplier(1).build());
+        setAttack(Property.builder().baseline(10).current(10).multiplier(1).build());
+        setAgility(Property.builder().baseline(4).current(4).multiplier(1).build());
+        setDefense(Property.builder().baseline(12).current(12).multiplier(1).build());
+        setIntelligence(Property.builder().baseline(6).current(6).multiplier(1).build());
+        setCharisma(Property.builder().baseline(6).current(6).multiplier(1).build());
+    }
+}
