bd03bc2f5b8e9799cd5a70e6534b602f76115c89
ENIX1701 <enix1701@gmail.com>
2024-01-14 12:02:23 +0100

    Add Eternal Dynamo

--- src/main/java/pl/umcs/items/EternalDynamo.java
+++ src/main/java/pl/umcs/items/EternalDynamo.java
@@ -1,2 +1,9 @@
-package pl.umcs.items;public class EternalDynamo {
+package pl.umcs.items;
+
+public class EternalDynamo extends Item {
+    public EternalDynamo() {
+        setName("Eternal Dynamo");
+        setSymbol('Ø');
+        setDescription("This is it. Everything we came here for, right before our eyes.");
+    }
 }
--- src/main/java/pl/umcs/items/Item.java
+++ src/main/java/pl/umcs/items/Item.java
@@ -1,6 +1,7 @@
-package pl.umcs;
+package pl.umcs.items;
 
 import lombok.*;
+import pl.umcs.GameElement;
 
 @Getter
 @Setter
